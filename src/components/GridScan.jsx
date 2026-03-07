a0e371d25115e4711fcf4f23fe197071309e7143
SamaKool <kulkarnisrk01@gmail.com>
2026-03-07 14:09:30 +0530

    Fixing merge conflicts

--- src/components/GridScan.jsx
+++ src/components/GridScan.jsx
@@ -564,6 +564,7 @@ export const GridScan = ({
                 composerRef.current.dispose();
                 composerRef.current = null;
             }
+            renderer.forceContextLoss();
             renderer.dispose();
             container.removeChild(renderer.domElement);
         };
