7ad4f278f46c5f0ffb8ce0791338771d9cb231b2
Aaditya-2407 <kharataaditya2407@gmail.com>
2026-03-17 11:51:05 +0530

    vite

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import { defineConfig } from 'vite'
-import react from '@vitejs/react-refresh'
+
 
 // https://vite.dev/config/
 export default defineConfig({
